e4148a75ecd078230c45e23c13805e50795d06b3
milkds <evp@artpolymer.com>
2018-10-25 16:16:58 +0300

    enabled database write/update

--- src/main/java/toytec/ToyDao.java
+++ src/main/java/toytec/ToyDao.java
@@ -41,7 +41,7 @@ public class ToyDao {
 
     public static void updateItem(Session session, ToyItem item) {
         //updates
-      /*  Transaction transaction = null;
+        Transaction transaction = null;
         try {
             transaction = session.getTransaction();
             transaction.begin();
@@ -53,8 +53,8 @@ public class ToyDao {
             }
             System.out.println("couldn't save item to db "+ item.getItemName());
             e.printStackTrace();
-            System.exit(0);
-        }*/
+         //   System.exit(0);
+        }
     }
 
     public static ToyItem getItemByWebLink(String link, Session session, String categoryName) {
@@ -72,7 +72,7 @@ public class ToyDao {
     }
 
     public static void addNewItem(ToyItem toyItem, Session session) {
-      /*  Transaction transaction = null;
+        Transaction transaction = null;
         try {
             transaction = session.getTransaction();
             transaction.begin();
@@ -91,8 +91,8 @@ public class ToyDao {
             }
             System.out.println("couldn't save car to db "+ toyItem.getItemName());
             e.printStackTrace();
-            System.exit(0);
-        }*/
+           // System.exit(0);
+        }
     }
 
     public static List<String> getItemLinksFromSubCategory(String subCategoryName, Session session) {
